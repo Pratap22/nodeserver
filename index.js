c40e4dc1928b360697d1cc2743e8fe2eb792b9eb
Pratap Sharma <sharma.pratap22@gmail.com>
2019-08-20 08:55:17 +0530

    React class 13 Node backend

--- index.js
+++ index.js
@@ -2,28 +2,57 @@
 const express = require('express');
 const bodyParser = express.json();
 const home = require('./routes/homeRouter');
+const mongoose = require('mongoose');
+const Todo = require('./model/TodoList');
 
 function main() {
   const app = express();
   const port = process.env.PORT || 8000;
   app.use(bodyParser);
+  mongoose.connect(
+    'mongodb+srv://pratap:patanahi@cluster0-ae91p.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true },
+    () => console.log('Database connected')
+  );
 
-  app.use('/api/', home);
+  app.post('/add', (req, res) => {
+    const { title, is_completed } = req.body;
+    const newTodo = new Todo({
+      title: title,
+      is_completed: is_completed
+    });
 
-  app.get('/', function(req, res) {
-    console.log('Server called on Home');
-    res.send('Home called' + 'This time with nodemon');
+    newTodo
+      .save()
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(400).json({ message: err }));
   });
 
-  app.post('/:id/:name', (req, res) => {
-    console.log('Req ', req);
-    const param = req.params;
-    const body = req.body;
-    console.log('Body ', body, 'Param ', param);
-
-    console.log(param);
-    res.send({ body });
+  app.get('/get', async (req, res) => {
+    try {
+      const savedTodos = await Todo.find();
+      res.status(200).json(savedTodos);
+    } catch (err) {
+      res.status(400).json({ message: err });
+    }
   });
+
+  // app.use('/api/', home);
+
+  // app.get('/', function(req, res) {
+  //   console.log('Server called on Home');
+  //   res.send('Home called' + 'This time with nodemon');
+  // });
+
+  // app.post('/:id/:name', (req, res) => {
+  //   console.log('Req ', req);
+  //   const param = req.params;
+  //   const body = req.body;
+  //   console.log('Body ', body, 'Param ', param);
+
+  //   console.log(param);
+  //   res.send({ body });
+  // });
 
   // app.post()
   // app.put()
